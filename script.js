7bd1e874b22ef49d6d288149fba2ed88dc6c6da9
vianovi <silviadewinovianaputri@gmail.com>
2025-10-10 18:29:53 +0700

    up paket1

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function getParam(name) {
 const target = getParam("to");
 
 // Tentukan URL tujuan (LOGIKA INTI)
-const destination = links[target] || links["default"];
+const destination = links[target] || links["paket1"];
 
 // Update link manual (tombol) agar sesuai dengan tujuan akhir
 document.getElementById('manual-link').href = destination;
